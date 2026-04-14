c3b1e02e132b0553ebe963a2d07d3107cbd8986b
ayushkhareGitHub <ayush.khare179@gmail.com>
2026-04-14 21:45:09 +0530

    Updated/Changed files
    Hide To Win Game Levels Updated

--- Assets/GameUI.cs
+++ Assets/GameUI.cs
@@ -1,30 +1,76 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class GameUI : MonoBehaviour
 {
+    public GameObject startUI;
+    public GameObject pauseUI;
     public GameObject gameLoseUI;
     public GameObject gameWinUI;
+
     bool gameIsOver;
+    bool gameStarted = false;
+    bool isPaused = false;
 
     void Start()
     {
+        startUI.SetActive(true);
+        Time.timeScale = 0f;
+
         Guard.OnGuardHasSpottedPlayer += ShowGameLoseUI;
         FindObjectOfType<Player>().OnReachedEndOfLevel += ShowGameWinUI;
     }
 
     void Update()
     {
+        if (!gameStarted)
+        {
+            if (Input.GetKeyDown(KeyCode.Return))
+            {
+                StartGame();
+            }
+            return;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            TogglePause();
+        }
+
         if (gameIsOver)
         {
             if (Input.GetKeyDown(KeyCode.Space))
             {
-                SceneManager.LoadScene(0);
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
             }
         }
     }
+
+    void StartGame()
+    {
+        startUI.SetActive(false);
+        Time.timeScale = 1f;
+        gameStarted = true;
+    }
+
+    void TogglePause()
+    {
+        isPaused = !isPaused;
+
+        if (isPaused)
+        {
+            Time.timeScale = 0f;
+            pauseUI.SetActive(true);
+        }
+        else
+        {
+            Time.timeScale = 1f;
+            pauseUI.SetActive(false);
+        }
+    }
+
 
     void ShowGameWinUI()
     {
@@ -40,8 +86,19 @@ public class GameUI : MonoBehaviour
     {
         gameOverUI.SetActive(true);
         gameIsOver = true;
+
         Guard.OnGuardHasSpottedPlayer -= ShowGameLoseUI;
         FindObjectOfType<Player>().OnReachedEndOfLevel -= ShowGameWinUI;
+    }
+
+    public void LoadNextLevel()
+    {
+        int currentIndex = SceneManager.GetActiveScene().buildIndex;
+        int totalScenes = SceneManager.sceneCountInBuildSettings;
+
+        int nextIndex = (currentIndex + 1) % totalScenes;
+
+        SceneManager.LoadScene(nextIndex);
     }
 
     public void QuitGameButtonPressed()
